ad296de42d1e1387415105acc84a28caac63df10
David Olona <damilear@outlook.com>
2023-12-27 16:31:09 -0500

    Create hourly forecast module

---
+++ src/hourly-display.js
@@ -0,0 +1,46 @@
+/*
+[
+    forecastData.forecast.forecastday[0].hour
+  {
+    time_epoch: 1703653200,
+    time: '2023-12-27 00:00',
+    temp_c: 6.3,
+    temp_f: 43.4,
+    is_day: 0,
+    condition: {
+      text: 'Fog',
+      icon: '//cdn.weatherapi.com/weather/64x64/night/248.png',
+      code: 1135
+    },
+    wind_mph: 4.5,
+    wind_kph: 7.2,
+    wind_degree: 53,
+    wind_dir: 'NE',
+    pressure_mb: 1017,
+    pressure_in: 30.05,
+    precip_mm: 0.01,
+    precip_in: 0,
+    snow_cm: 0,
+    humidity: 98,
+    cloud: 100,
+    feelslike_c: 5,
+    feelslike_f: 41,
+    windchill_c: 5,
+    windchill_f: 41,
+    heatindex_c: 6.3,
+    heatindex_f: 43.4,
+    dewpoint_c: 6,
+    dewpoint_f: 42.7,
+    will_it_rain: 1,
+    chance_of_rain: 78,
+    will_it_snow: 0,
+    chance_of_snow: 0,
+    vis_km: 0,
+    vis_miles: 0,
+    gust_mph: 8.8,
+    gust_kph: 14.2,
+    uv: 1
+  },
+  ...
+]
+*/
