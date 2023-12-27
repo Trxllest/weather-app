1175135c49bbb7623f5327ae89cfd13ef9403d34
David Olona <damilear@outlook.com>
2023-12-27 16:31:27 -0500

    Get endpoint for 3day FC

---
+++ src/three-dayFC.js
@@ -0,0 +1,25 @@
+/*
+{
+  '0': {
+    date: '2023-12-27',
+    temp_c: 6.5,
+    temp_f: 43.8,
+    condition: 'Moderate rain',
+    icon: '//cdn.weatherapi.com/weather/64x64/day/302.png'
+  },
+  '1': {
+    date: '2023-12-28',
+    temp_c: 6.5,
+    temp_f: 43.6,
+    condition: 'Patchy rain possible',
+    icon: '//cdn.weatherapi.com/weather/64x64/day/176.png'
+  },
+  '2': {
+    date: '2023-12-29',
+    temp_c: 6.4,
+    temp_f: 43.6,
+    condition: 'Patchy rain possible',
+    icon: '//cdn.weatherapi.com/weather/64x64/day/176.png'
+  }
+}
+*/
