4ea9961b33fab8862c6a3e708b525e157a607c70
David Olona <damilear@outlook.com>
2024-01-03 23:13:39 -0500

    Add 3 Day forecast component

--- src/three-dayFC.js
+++ src/three-dayFC.js
@@ -1,25 +1,80 @@
 /*
-{
-  '0': {
+[
+  {
     date: '2023-12-27',
     temp_c: 6.5,
     temp_f: 43.8,
     condition: 'Moderate rain',
     icon: '//cdn.weatherapi.com/weather/64x64/day/302.png'
   },
-  '1': {
+  {
     date: '2023-12-28',
     temp_c: 6.5,
     temp_f: 43.6,
     condition: 'Patchy rain possible',
     icon: '//cdn.weatherapi.com/weather/64x64/day/176.png'
   },
-  '2': {
+  {
     date: '2023-12-29',
     temp_c: 6.4,
     temp_f: 43.6,
     condition: 'Patchy rain possible',
     icon: '//cdn.weatherapi.com/weather/64x64/day/176.png'
   }
-}
+]
 */
+
+import { processForecast } from "./get-weather";
+
+
+const dailyFC = document.querySelector(".three-day-forecast");
+const defaultLocation = "toronto";
+
+function forecastComponent(fObj) {
+//   const weekday = [
+//     "Sunday",
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thursday",
+//     "Friday",
+//     "Saturday",
+//   ];
+  fObj.forEach((d) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const day = document.createElement("h3");
+    const icon = document.createElement("div");
+    const temp = document.createElement("h1");
+
+    icon.classList.add("w-icon");
+    day.classList.add("desc");
+    temp.classList.add("temp");
+
+    day.textContent = d;
+    const dday = new Date(d.date);
+    day.textContent = dday.getDate();
+
+    temp.textContent = d.temp_c
+
+    const img = new Image();
+    img.src = d.icon;
+    icon.appendChild(img);
+
+    card.appendChild(day);
+    card.appendChild(icon);
+    card.appendChild(temp);
+
+    dailyFC.appendChild(card);
+  });
+}
+
+async function getFCData(location=defaultLocation) {
+  const fcWeather = await processForecast(location);
+  console.log(fcWeather);
+  dailyFC.textContent = "";
+  forecastComponent(fcWeather);
+}
+
+export {getFCData};
