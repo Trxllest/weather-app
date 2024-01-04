ec6eb08f1c9321241943380a94cee70edeb2d712
David Olona <damilear@outlook.com>
2024-01-03 23:14:16 -0500

    Import components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import "./style.css";
-import {getData, displayCurr} from "./curr-display";
+import {getCurrData} from "./curr-display";
+import { getFCData } from "./three-dayFC";
+import { getHrlyData } from "./hourly-display";
 
-getData();
+const searchIcon = document.querySelector(".search-icon");
+const searchInput = document.querySelector("#search");
+
+getCurrData();
+getFCData();
+getHrlyData();
+
+function displayCurr() {
+  searchIcon.addEventListener("click", () => {
+    if (searchInput.value !== "") {
+      getCurrData(searchInput.value);
+      getFCData(searchInput.value);
+      getHrlyData(searchInput.value);
+    }
+  });
+}
+
 displayCurr();
