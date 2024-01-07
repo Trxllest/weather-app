508925a39c22eb222e5ca2ce08a95c0ded33401d
David Olona <damilear@outlook.com>
2024-01-07 17:51:14 -0500

    Add gif

--- src/gif.js
+++ src/gif.js
@@ -1,4 +1,6 @@
 // Globals
+import { processCurrData } from "./get-weather";
+
 const API_KEY = "DVrRXnEwn0X9AY4F6pEbLXuFKWxtVHHj";
 const BASE_URL = "http://api.giphy.com/v1/gifs/search";
 
@@ -13,3 +15,16 @@ async function getGifData(weather) {
 }
 
 getGifData("Cloudy");
+
+export default async function setGif(location) {
+  const weather = await processCurrData(location);
+  const data = await getGifData(weather.condition);
+  const gifLink = data.data[0].images.original.url;
+  console.log(gifLink);
+
+  const currSxn = document.querySelector(".current-weather");
+  currSxn.style.background = `url(${gifLink}) center/cover no-repeat`;
+  currSxn.style.height = data.data[0].images.original.height;
+}
+
+setGif("Toronto");
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./style.css";
 import {getCurrData} from "./curr-display";
 import { getFCData } from "./three-dayFC";
 import { getHrlyData } from "./hourly-display";
+import setGif from "./gif";
 
 const searchIcon = document.querySelector(".search-icon");
 const searchInput = document.querySelector("#search");
@@ -9,15 +10,26 @@ const searchInput = document.querySelector("#search");
 getCurrData();
 getFCData();
 getHrlyData();
+setGif("toronto")
 
 function displayCurr() {
-  searchIcon.addEventListener("click", () => {
-    if (searchInput.value !== "") {
+  const fetchData = () => {
+    if (searchInput.value.trim() !== "") {
       getCurrData(searchInput.value);
       getFCData(searchInput.value);
       getHrlyData(searchInput.value);
+      setGif(searchInput.value);
     }
+  };
+
+  searchIcon.addEventListener("click", fetchData);
+
+  // Use 'submit' event on the form to prevent page refresh
+  document.querySelector("form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    fetchData();
   });
 }
+
 
 displayCurr();
